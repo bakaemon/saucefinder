f2780cd485d86a966fd17960d70e4b247ac731e1
bakaemon <69375806+bakaemon@users.noreply.github.com>
2021-05-10 11:41:52 +0700

    overhaul UI, added bookmark feature

--- saucefind.py
+++ saucefind.py
@@ -12,6 +12,37 @@ else:
     page = int(args.page)
 
 
+def browser(url):
+    os.system(
+        'start shell:AppsFolder\Microsoft.MicrosoftEdge_8wekyb3d8bbwe!MicrosoftEdge -private ' +
+        url
+    )
+
+
+def record(data: list):
+    with open("saves.txt", "w", encoding="utf-8") as f:
+        f.write("\n".join(data))
+
+
+def readCodes():
+    with open("saves.txt", "r", encoding="utf-8") as f:
+        return f.read().split("\n")
+
+
+def isCodeEmpty():
+    with open("saves.txt", "r", encoding="utf-8") as f:
+        line = f.read(1)
+        if not line:
+            return True
+        else:
+            return False
+
+
+def deleteCodes():
+    with open("saves.txt", "w", encoding="utf-8") as f:
+        f.truncate(0)
+        f.close()
+
 def yn(prompt: str):
     negative = True
     res = ""
@@ -34,35 +65,72 @@ def saucefind(text: str, page=1):
     results_html = soup.find('h1')
     names = [x.text for x in names_html]
     results = results_html.text
-    links = [f'https://nhentai.net{x.get("href")}' for x in links_html]
-    print("Found" + results)
-    for i in range(len(names)):
-        print(f"{i + 1}. {names[i]}\nLink:{links[i]}\n")
+    links = [x.get("href").split("/")[2] for x in links_html]
     run = True
     while run:
-        print("\n1. Change page.\n2. Enjoy one in incognito.\n")
+        os.system("cls")
+        print("Found" + results+"|| Search results of \""+text+"\"")
+        print("*=============================================*")
+        for i in range(len(names)):
+            print(f"{i + 1}. [{links[i]}]     {names[i]}\n")
+        print("*=============================================*")
+        print(f"Current page: {str(page)}\n")
+        print("\n1. Change page.\n2. Enjoy one in incognito.\n3. Save one to bookmark\n4. Check bookmarks\n5. Clear "
+              "bookmarks\n0.Exit")
         choice = int(input("Option: "))
         if choice == 0:
             run = False
         elif choice == 1:
-            page_num = int(input(f"Current page: {str(page)}\nChoose page: "))
+            page_num = int(input(f"Choose page: "))
             if page_num <= 0:
                 run = True
             else:
                 saucefind(text, page=page_num)
         elif choice == 2:
-            i = int(input("Which one you want to read (Enter number)?"))
+            i = int(input("Which one you want to read (Enter number)?\n"))
             if 0 < i <= len(links):
-                isPositive = yn("Would you like to read it on incognito mode? (y/n)\n")
+                isPositive = yn("Are you sure to read it on incognito mode? (y/n)\n")
                 if isPositive:
-                    os.system(
-                        'start shell:AppsFolder\Microsoft.MicrosoftEdge_8wekyb3d8bbwe!MicrosoftEdge -private ' +
-                        links[i - 1]
-                    )
-                    run = False
+                    url = "https://nhentai.net/g/" + links[i - 1]
+                    browser(url)
+                    input("Press any keys to continue...")
                 else:
                     print("Can't find the number.")
+                    input("Press any keys to continue...")
                     run = True
+        elif choice == 3:
+            io = input("Enter a list of index number separated by whitespace: \n")
+            indexes = io.split(" ")
+            data = [f"{links[int(i)-1]}    {names[int(i)-1]}" for i in indexes]
+
+            record(data)
+        elif choice == 4:
+            data = readCodes()
+            code = []
+            print("Saved in saves.txt:\n")
+            if isCodeEmpty():
+                print("There is no code in bookmark.\n")
+                input("Press any keys to continue...")
+            else:
+                n = 1
+                for x in data:
+                    print(str(n)+". "+x, sep="\n")
+                    code.append(x.split("    ")[0])
+                    n += 1
+                pr = yn("Would you like to read any?(y/n)\n")
+                if pr:
+                    i = int(input("Input the index to read: "))
+                    if 0 < i-1 < len(code):
+                        browser("https://nhentai.net/g/"+code[i-1])
+                        input("Press any keys to continue...")
+                    else:
+                        print("Can't find index number.\n")
+                        input("Press any keys to continue...")
+        elif choice == 5:
+            if yn("Are you sure to delete everything? (y/n)\n"):
+                deleteCodes()
+                print("Clear-up successfully.\n")
+            input("Press any keys to continue...")
     print("Process ended.")
 
 
